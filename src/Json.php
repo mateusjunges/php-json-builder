7aad4a08a4783c8f514b7d3f21c76b1ebea13de6
Mateus Junges <mateus@junges.dev>
2022-06-30 22:29:28 -0300

    create json class

---
+++ src/Json.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Junges\Json;
+
+use ArrayAccess;
+use ArrayIterator;
+use IteratorAggregate;
+use JsonSerializable;
+use Stringable;
+use Traversable;
+
+class Json implements Stringable, ArrayAccess, JsonSerializable, IteratorAggregate
+{
+
+}
